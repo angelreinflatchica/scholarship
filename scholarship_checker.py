e03c4628ba178b4e97d637eccb2fb01d1a176846
angelreinflatchica <angelreinlatchica@gmail.com>
2025-05-16 20:32:01 +0800

    update scholarship_checker.py and test_scholarship_checker.py

--- scholarship_checker.py
+++ scholarship_checker.py
@@ -1,7 +1,13 @@
 def is_eligible_for_scholarship(gpa, family_income):
+    
     if gpa < 0.0 or gpa > 4.0:
         raise ValueError("Invalid GPA")
     if family_income < 0:
         raise ValueError("Income cannot be negative")
     
     return gpa >= 3.5 and family_income <= 20000
+
+def is_low_gpa (gpa):
+    if gpa <1.0 or gpa >3.5:
+        raise ValueError("Low GPA")
+    
--- test_scholarship_checker.py
+++ test_scholarship_checker.py
@@ -1,7 +1,12 @@
 import unittest
 from scholarship_checker import is_eligible_for_scholarship
 
+
 class TestScholarshipEligibility(unittest.TestCase):
 
     def test_eligible_student(self):
         self.assertTrue(is_eligible_for_scholarship(3.8, 15000))
+        self.assertFalse(is_eligible_for_scholarship (3.6, 21000 ))
+
+    def test_low_gpa(self):
+        self.assertTrue(is_low_gpa (3.8, 15000))
